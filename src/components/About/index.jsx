3e6b938830810849aa16d143ba15b4d4e683d3c7
EdsonCampanhao <edsonlemos0525@gmail.com>
2023-05-09 14:23:42 -0300

    att about component
    some changes in text and img

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,12 +5,14 @@ export default function About() {
         <section className='about__container'>
             <div className="about__img"></div>
             <div className="about__content">
-                <h1 className="about__title">Sua Nutricionista especialista</h1>
+                <h1 className="about__title">Andrea barcelos</h1>
                 <p className="about__text">
-                    Olá! Sou a Andrea Barcelos, sua nova nutricionista!
+                    Olá!  Sou Nutricionista há mais de 15 anos, formada e especializada pela Universidade Federal Fluminense. Mestre em Atenção a Saúde da Criança
+                    e Na área de Terapia Alternativa. Reikiana Nível 2 e Especialista em Terapia Floral, pelo Instituto Brasileiro de  Pesquisas Homeopaticas.
                     Estou aqui para ajudá-lo(a) a desenvolver um plano de dieta personalizado que se adeque perfeitamente ao seu perfil e aos seus objetivos.
                     Juntos, vamos trabalhar para que você descubra a melhor versão de si mesmo(a), obtendo resultados físicos de nível profissional e, ao mesmo tempo,
-                    melhorando sua saúde e autoestima. Estou empolgada para embarcar nessa jornada com você!
+                    melhorando sua saúde e autoestima.
+
                 </p>
             </div>
         </section>
