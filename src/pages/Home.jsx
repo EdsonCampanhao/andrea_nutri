a2227f9b39286fbf9320df89af3dfb5b3d83aeae
EdsonCampanhao <108037292+EdsonCampanhao@users.noreply.github.com>
2023-10-14 07:12:47 -0300

    att

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Specialties from "../components/Specialties";
 import Treatments from "../components/Treatments";
 import WppButton from '../components/WppButton'
 
+
 export default function Home (){
     return(
         <>
         <WppButton number='+552196402-6412' />
        <About/>
+       
        <Specialties/>
        <Treatments/>
        <Faq/>
