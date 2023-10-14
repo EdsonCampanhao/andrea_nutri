c2724e2640e17e99049f9ef189e6427eee16d309
EdsonCampanhao <108037292+EdsonCampanhao@users.noreply.github.com>
2023-10-14 08:16:30 -0300

    att

--- src/components/ArticleField/Article/index.jsx
+++ src/components/ArticleField/Article/index.jsx
@@ -4,10 +4,15 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+
 import './Article.css'
 
 export default function Article(props) {
+
+
     return (
+
+
         <Card sx={{ maxWidth: 345 }} className='article' >
             <CardActionArea>
                 <CardMedia
